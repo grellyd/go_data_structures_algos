788cd74d7f1d4826eb036d537ecab2393de4ab35
grellyd <graham.brown137@gmail.com>
2018-02-21 23:30:30 -0800

    Restructured tests to share test cases

--- sorting/sorting_test.go
+++ sorting/sorting_test.go
@@ -3,37 +3,42 @@ package sorting
 import (
 	"testing"
 )
+	
+var tests = []struct {
+	input []int
+	output []int
+} {
+	{
+		[]int{},
+		[]int{},
+	},
+	{
+		[]int{1,2,3},
+		[]int{1,2,3},
+	},
+	{
+		[]int{3,2,1},
+		[]int{1,2,3},
+	},
+	{
+		[]int{4,3,2,1},
+		[]int{1,2,3,4},
+	},
+	{
+		[]int{3,1,2},
+		[]int{1,2,3},
+	},
+	{
+		[]int{3,7,1,2,4,9,5,10,8,6},
+		[]int{1,2,3,4,5,6,7,8,9,10},
+	},
+	{ 
+		[]int{-9,-4,-2,-7},
+		[]int{-9,-7,-4,-2},
+	},
+}
 
 func TestQuicksort(t *testing.T) {
-	var tests = []struct {
-		input []int
-		output []int
-	} {
-		{
-			[]int{},
-			[]int{},
-		},
-		{
-			[]int{1,2,3},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,2,1},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,1,2},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,7,1,2,4,9,5,10,8,6},
-			[]int{1,2,3,4,5,6,7,8,9,10},
-		},
-		{ 
-			[]int{-9,-4,-2,-7},
-			[]int{-9,-7,-4,-2},
-		},
-	}
 	for _, test := range tests {
 		result := Quicksort(test.input)
 		if len(result) != len(test.output) {
@@ -49,35 +54,6 @@ func TestQuicksort(t *testing.T) {
 
 
 func TestMergesort(t *testing.T) {
-	var tests = []struct {
-		input []int
-		output []int
-	} {
-		{
-			[]int{},
-			[]int{},
-		},
-		{
-			[]int{1,2,3},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,2,1},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,1,2},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,7,1,2,4,9,5,10,8,6},
-			[]int{1,2,3,4,5,6,7,8,9,10},
-		},
-		{ 
-			[]int{-9,-4,-2,-7},
-			[]int{-9,-7,-4,-2},
-		},
-	}
 	for _, test := range tests {
 		result := Mergesort(test.input)
 		if len(result) != len(test.output) {
@@ -94,35 +70,6 @@ func TestMergesort(t *testing.T) {
 
 
 func TestBubblesort(t *testing.T) {
-	var tests = []struct {
-		input []int
-		output []int
-	} {
-		{
-			[]int{},
-			[]int{},
-		},
-		{
-			[]int{1,2,3},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,2,1},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,1,2},
-			[]int{1,2,3},
-		},
-		{
-			[]int{3,7,1,2,4,9,5,10,8,6},
-			[]int{1,2,3,4,5,6,7,8,9,10},
-		},
-		{ 
-			[]int{-9,-4,-2,-7},
-			[]int{-9,-7,-4,-2},
-		},
-	}
 	for _, test := range tests {
 		result := Bubblesort(test.input)
 		if len(result) != len(test.output) {
